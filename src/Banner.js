851f6b0551624cd686a7c0e3a9d13e886364b8e3
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-08-22 17:22:08 +0530

    Added Banner.js component

---
+++ src/Banner.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Banner.css';
+
+function Banner() {
+
+    function truncate(string, n) {
+        return string?.length > n ? string.substr(0, n-1) + '...' : string
+    }
+
+    return (
+        <header className="banner" style={{
+            backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+            backgroundPosition: "center center"
+        }}>
+            <div className="banner__contents">
+                <h1 className="banner__title">Movie Name</h1>
+                <div className="banner__buttons">
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
+                </div>
+                <h1 className="banner__description">
+                    {truncate("This is a test description", 15)}
+                </h1>  
+            </div>
+
+            <div className="banner--fadeBottom" />
+             
+        </header>
+    )
+}
+
+export default Banner
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import './HomeScreen.css'
 import Nav from './Nav'
+import Banner from './Banner'
 
 export default function HomeScreen() {
     return (
         <div className="homeScreen">
             <Nav />
+            <Banner />
         </div>
     )
 }
