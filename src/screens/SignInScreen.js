73ca0535c53bc3aa3fed86b282d00043b4c34fc9
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-08-23 17:38:20 +0530

    Added SignInScreen Componenet

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
+import './SignInScreen.css'
 function SignInScreen() {
+
+    const register = (e) => {
+        e.preventDefault();
+    }
+
+    const signIn = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div className="sign">
+        <div className="signUpScreen">
+            <form>
+                <h1>Sign In</h1>
+                <input placeholder="Email" type="email" />
+                <input placeholder="Password" type="password" />
+                <button type="submit" onClick={signIn}>Sign In</button>
+
+                <h4>
+                    <span className="signUpScreen__gray">New to Netflix? Sign? </span>
+                    <span className="signUpScreen__link" onClick={register}>Sign Up now.</span>
+                </h4>
+            </form>
             
         </div>
     )
